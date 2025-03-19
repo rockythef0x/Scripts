a4aa084cd64e9c425a69be52b8a0f69fb5fc7876
Rocky Rickaby <81652934+rocky-rickaby10@users.noreply.github.com>
2025-03-18 22:19:17 -0700

    Create NoLaserDeath.lua

---
+++ NoLaserDeath.lua
@@ -0,0 +1,11 @@
+for _, v in ipairs(workspace.Tycoons["Tycoons"]:GetDescendants()) do
+    if v.Name == "Pipe" then
+        v.CanTouch = false
+    end
+end
+
+workspace.Tycoons["Tycoons"].DescendantAdded:Connect(function(descendant)
+    if descendant.Name == "Pipe" then
+        descendant.CanTouch = false
+    end
+end)
