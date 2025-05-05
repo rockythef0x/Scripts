f67aad9e74b0dbddd29fe6bff2600c861763d141
Rocky Rickaby <81652934+rocky-rickaby10@users.noreply.github.com>
2025-05-04 20:17:38 -0700

    Make no storm gui remove fog effect

--- ndsscript.lua
+++ ndsscript.lua
@@ -1,3 +1,4 @@
+local Lighting = game:GetService("Lighting")
 local localPlr = game:GetService("Players").LocalPlayer
 if getgenv().ndsscripthasRun then
     return
@@ -54,6 +55,13 @@ localPlr.PlayerGui.ChildAdded:Connect(function(gui)
     if getgenv().removestormGuis == true then
         if gui.Name == "SandStormGui" or gui.Name == "BlizzardGui" then
             gui.Enabled = false
+			task.wait(1.5)
+			Lighting.FogEnd = 100000
+			for i,v in pairs(Lighting:GetDescendants()) do
+				if v:IsA("Atmosphere") then
+					v:Destroy()
+				end
+			end
         end
     end
 end)
