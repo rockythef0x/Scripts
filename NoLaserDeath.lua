9640d1f876e0db5c90605da388ae1b0bc6d9bd27
Rocky Rickaby <81652934+rocky-rickaby10@users.noreply.github.com>
2025-05-05 14:42:28 -0700

    Update NoLaserDeath.lua

--- NoLaserDeath.lua
+++ NoLaserDeath.lua
@@ -1,11 +1,13 @@
+local tycoons = workspace.Tycoons.Tycoons
+
 game:GetService("StarterGui"):SetCore("SendNotification", {Title = "No Laser Death", Text = "No Laser Death Enabled!"})
-for _, v in ipairs(workspace.Tycoons["Tycoons"]:GetDescendants()) do
+for _, v in ipairs(tycoons:GetDescendants()) do
     if v.Name == "Pipe" then
         v.CanTouch = false
     end
 end
 
-workspace.Tycoons["Tycoons"].DescendantAdded:Connect(function(descendant)
+tycoons.DescendantAdded:Connect(function(descendant)
     if descendant.Name == "Pipe" then
         descendant.CanTouch = false
     end
