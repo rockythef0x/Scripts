42c077121656f674ac6675a6de23331494742b4c
Rocky Rickaby <81652934+rocky-rickaby10@users.noreply.github.com>
2025-05-09 12:08:00 -0700

    Update ndsscript.lua
    Use a table for configuration

--- ndsscript.lua
+++ ndsscript.lua
@@ -1,25 +1,30 @@
 local Lighting = game:GetService("Lighting")
 local localPlr = game:GetService("Players").LocalPlayer
+
 if getgenv().ndsscripthasRun then
     return
 else
     getgenv().ndsscripthasRun = true
 end
 
--- Default settings if they didn't get set
--- I know my method to do this is ass
-if getgenv().predictEnabled == nil then
-    getgenv().predictEnabled = true
-end
-if getgenv().chatMessage == nil then
-    getgenv().chatMessage = false
-end
-if getgenv().noStormEffect == nil then
-    getgenv().noStormEffect = true
-end
-if getgenv().notifyTime == nil or not tonumber(getgenv().notifyTime) then
-	getgenv().notifyTime = 10
-end
+local defaultSettings = {
+    ["PredictEnabled"] = true, 
+    ["ChatMessage"] = false,
+    ["NoStorms"] = true,
+    ["NotifyTime"] = 15
+}
+
+-- Make sure the settings exist in case the loadstring is outdated
+if getgenv().ndsSettings then
+    for i, v in pairs(defaultSettings) do
+        if not ndsSettings[i] then
+            ndsSettings[i] = v
+        end
+    end
+else
+    getgenv().ndsSettings = table.clone(defaultSettings)
+end 
+
 
 local function notifyUser(title, text, duration)
     game:GetService("StarterGui"):SetCore("SendNotification", {Title = title, Text = text, Duration = duration})
@@ -32,9 +37,9 @@ local function thingyidk()
     local char = localPlr.Character or localPlr.CharacterAdded:Wait()
     addedConnection = char.ChildAdded:Connect(function(child)
         local function predict()
-            if getgenv().predictEnabled == true then
-                notifyUser("Next Disaster", "Next Disaster is: " .. child.Value, getgenv().notifyTime)
-                if getgenv().chatMessage == true then
+            if ndsSettings.PredictEnabled == true then
+                notifyUser("Next Disaster", "Next Disaster is: " .. child.Value, ndsSettings.NotifyTime)
+                if ndsSettings.ChatMessage == true then
                     game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Next Disaster is: " .. child.Value)
                 end
             end
@@ -55,13 +60,13 @@ localPlr.CharacterRemoving:Connect(function()
 end)
 
 Lighting:GetPropertyChangedSignal("FogEnd"):Connect(function()
-    if getgenv().noStormEffect then
+    if ndsSettings.NoStorms then
         Lighting.FogEnd = 100000
     end
 end)
 
 localPlr.PlayerGui.ChildAdded:Connect(function(gui)
-    if getgenv().noStormEffect == true then
+    if ndsSettings.NoStorms == true then
         if gui.Name == "SandStormGui" or gui.Name == "BlizzardGui" then
             gui.Enabled = false
         end
