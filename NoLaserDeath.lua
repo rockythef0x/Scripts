b38e33c7cf41da1eff2b6a7568c340f66c2fce1c
Rocky Rickaby <81652934+rocky-rickaby10@users.noreply.github.com>
2025-05-31 19:14:54 -0700

    Micro Optimize NoLaserDeath

--- NoLaserDeath.lua
+++ NoLaserDeath.lua
@@ -1,14 +1,12 @@
+if getgenv().noDeathEnabled == false and game.PlaceId == 5656638348 then getgenv().noDeathEnabled = true else return end
 local tycoons = workspace.Tycoons.Tycoons
 
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = "No Laser Death", Text = "No Laser Death Enabled!"})
-for _, v in ipairs(tycoons:GetDescendants()) do
-    if v.Name == "Pipe" then
-        v.CanTouch = false
-    end
-end
+local function noDeath(part) if part.Name == "Pipe" then part.CanTouch = false end end
+tycoons.DescendantAdded:Connect(noDeath)
+for _, v in ipairs(tycoons:GetDescendants()) do noDeath(v) end
 
-tycoons.DescendantAdded:Connect(function(descendant)
-    if descendant.Name == "Pipe" then
-        descendant.CanTouch = false
-    end
-end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "No Laser Death",
+    Text = "No Laser Death Enabled!",
+    Duration = 3
+})
