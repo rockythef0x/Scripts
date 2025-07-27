415ee865e6c80b127f24748e82ce2d52e709f27b
Rocky Rickaby <81652934+rockythef0x@users.noreply.github.com>
2025-07-27 14:43:37 -0700

    Update ndsscript.lua

--- ndsscript.lua
+++ ndsscript.lua
@@ -30,13 +30,13 @@ local function notifyUser(title, text, duration)
 end
 
 notifyUser("Prediction Enabled", "Disaster Prediction has been enabled!", 3)
-local predictConnection = nil
-local addedConnection = nil
-local function thingyidk()
+local predictConnection
+local addedConnection
+local function main()
     local char = localPlr.Character or localPlr.CharacterAdded:Wait()
     addedConnection = char.ChildAdded:Connect(function(child)
         local function predict()
-            if ndsSettings.PredictEnabled == true then
+            if ndsSettings.PredictEnabled then
                 notifyUser("Next Disaster", "Next Disaster is: " .. child.Value, ndsSettings.NotifyTime)
                 if ndsSettings.ChatMessage == true then
                     game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Next Disaster is: " .. child.Value)
@@ -50,9 +50,9 @@ local function thingyidk()
         end
     end)
 end
-thingyidk()
+main()
 
-localPlr.CharacterAdded:Connect(thingyidk)
+localPlr.CharacterAdded:Connect(main)
 localPlr.CharacterRemoving:Connect(function()
     addedConnection:Disconnect()
     if predictConnection then predictConnection:Disconnect() end
@@ -66,7 +66,7 @@ end)
 
 localPlr.PlayerGui.ChildAdded:Connect(function(gui)
     if ndsSettings.NoStorms == true then
-        if gui.Name == "SandStormGui" or gui.Name == "BlizzardGui" then
+        if table.find({"SandStormGui", "BlizzardGui"}, gui.Name} then
             gui.Enabled = false
         end
     end
