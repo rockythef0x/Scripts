497c8470c20b871e44e77f963a320f37fca1c986
Rocky <81652934+rockythef0x@users.noreply.github.com>
2026-04-18 19:12:47 -0700

    Convert to use getgenv config

--- ShiftToRun.lua
+++ ShiftToRun.lua
@@ -9,8 +9,7 @@ local humanoid = char:WaitForChild("Humanoid")
 local mouseLockController = cloneref(plr.PlayerScripts:WaitForChild("PlayerModule"):WaitForChild("CameraModule"):WaitForChild("MouseLockController"))
 local boundKeys = cloneref(mouseLockController:FindFirstChild("BoundKeys"))
 
--- Configurations
-local config = {
+getgenv().run_config = {
 	["FovChange"] = true, -- Whether to do the zoomed out FOV effect while running (default is true)
 	["Fov"] = 80, -- What the sprinting FOV will be (Roblox default is 70, script default is 80)
 	["ChangeTime"] = 0.40, -- Adjusts the time for the speed and FOV change (script default is 0.40)
@@ -18,7 +17,7 @@ local config = {
 	["UseCtrlLock"] = true -- Whether to bind Shiftlock to Ctrl so it doesn't conflict with sprinting (default is true)
 }
 
-if config.UseCtrlLock then
+if run_config.UseCtrlLock then
 	if boundKeys then
 		boundKeys.Value = "LeftControl"
 	else
@@ -30,14 +29,14 @@ if config.UseCtrlLock then
 end
 
 local oldFov = workspace.CurrentCamera and workspace.CurrentCamera.FieldOfView
-local tweenInfo = TweenInfo.new(config.ChangeTime, Enum.EasingStyle.Linear, Enum.EasingDirection.In)
+local tweenInfo = TweenInfo.new(run_config.ChangeTime, Enum.EasingStyle.Linear, Enum.EasingDirection.In)
 UserInputService.InputBegan:Connect(function(input, processed)
 	if input.KeyCode == Enum.KeyCode.LeftShift and not processed then
-		local speed = TweenService:Create(humanoid, tweenInfo, {["WalkSpeed"] = config.Speed})
+		local speed = TweenService:Create(humanoid, tweenInfo, {["WalkSpeed"] = run_config.Speed})
 		speed:Play()
 
-		if config.FovChange == true and workspace.CurrentCamera then
-			local tween = TweenService:Create(workspace.CurrentCamera, tweenInfo, {["FieldOfView"] = config.Fov})
+		if run_config.FovChange == true and workspace.CurrentCamera then
+			local tween = TweenService:Create(workspace.CurrentCamera, tweenInfo, {["FieldOfView"] = run_config.Fov})
 			tween:Play()
 		end
 	end
@@ -48,7 +47,7 @@ UserInputService.InputEnded:Connect(function(input, processed)
 		local speed = TweenService:Create(humanoid, tweenInfo, {["WalkSpeed"] = game:GetService("StarterPlayer").CharacterWalkSpeed})
 		speed:Play()
 
-		if config.FovChange == true and workspace.CurrentCamera and oldFov then
+		if run_config.FovChange == true and workspace.CurrentCamera and oldFov then
 			local tween = TweenService:Create(workspace.CurrentCamera, tweenInfo, {["FieldOfView"] = oldFov})
 			tween:Play()
 		end
